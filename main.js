8857a90919ee352da1b83c47cb5e5729bcdc27af
suryar0506 <72110042+suryar0506@users.noreply.github.com>
2021-04-08 03:11:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("hello");
+Webcam.set({
+width: 350,
+height: 300,
+img_format: 'png',
+png_quality: 90
+});
+
+camera = document.getElementById("webcam");
+
+Webcam.attach('#webcam');
+
+function takeSnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("snapshot").innerHTML = '<img id = "captured_image" src = "'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version : ', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/GA0gaOtOm/model.json',modelLoaded);
