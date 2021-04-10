fd054c767a6b8632c103361717d2e42d6137c104
suryar0506 <72110042+suryar0506@users.noreply.github.com>
2021-04-11 02:57:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,3 +17,22 @@ function takeSnapshot(){
 
 console.log('ml5 version : ', ml5.version);
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/GA0gaOtOm/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+}
+
+function identifySnapshot(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+if (error){
+    console.error(error);
+} else {
+    console.log(results);
+    document.getElementById("object").innerHTML = results[0].label;
+    document.getElementById("accuracy").innerHTML = results[0].confidence.toFixed(3);
+}
+}
